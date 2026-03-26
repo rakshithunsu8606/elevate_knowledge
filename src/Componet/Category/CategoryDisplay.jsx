119470277ef7a0ed5146697656374e0ec29b62a2
Rakshit Hinsu <rakshithinsu8606@gmail.com>
2026-03-26 23:23:48 +0530

    kkk

--- src/Componet/Category/CategoryDisplay.jsx
+++ src/Componet/Category/CategoryDisplay.jsx
@@ -45,6 +45,14 @@ function CategoryDisplay(props) {
 
     return (
         <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+            <div className="nav my-3 my-xl-0 px-4 flex-nowrap align-items-center">
+                <div className="nav-item w-100">
+                    <form className="position-relative">
+                        <input className="form-control pe-5 bg-transparent" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn bg-transparent px-2 py-0 position-absolute top-50 end-0 translate-middle-y" type="submit"><i className="fas fa-search fs-6 " /></button>
+                    </form>
+                </div>
+            </div>
             {
                 Category.category?.map((v) => (
                     <Card sx={{ maxWidth: 345 }} >
