d56db8c01e3fd32d3d9012408e7e21559ced7cf9
Rakshit Hinsu <rakshithinsu8606@gmail.com>
2026-03-27 11:20:18 +0530

    hah

--- utility/url.js
+++ utility/url.js
@@ -1,9 +1,9 @@
+// export const BASE_URL="http://localhost:8080/api/v1/"
+
+// export const IMAGE_URL="http://localhost:8080/"
+
+
+
 export const BASE_URL="https://learn-node-js-ciu4.vercel.app/api/v1/"
 
 export const IMAGE_URL="https://learn-node-js-ciu4.vercel.app/"
-
-
-
-// export const BASE_URL="https://learn-node-js-ciu4.vercel.app/api/v1/"
-
-// export const IMAGE_URL="https://learn-node-js-ciu4.vercel.app/"
